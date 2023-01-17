e37216a90c1c65d623b29344add0e6a0987e08ca
kweeka <mtalbi29@yahoo.com>
2023-01-17 13:59:46 +0100

    Add transactions and more queries

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-/*Queries that provide answers to the questions from all projects.*/
+-- Queries
 
 SELECT * from animals WHERE name LIKE '%mon';
 SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
@@ -8,3 +8,83 @@ SELECT (name, escape_attempts) from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Transaction
+
+BEGIN TRANSACTION;
+
+UPDATE animals SET species = 'unspecified';
+SELECT * from animals WHERE species = 'unspecified';
+
+ROLLBACK;
+
+SELECT * from animals WHERE species = '';
+
+-- Transaction
+
+BEGIN TRANSACTION;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species = '';
+
+COMMIT;
+
+SELECT * from animals WHERE species = 'digimon';
+SELECT * from animals WHERE species = 'pokemon';
+
+-- Transaction
+
+BEGIN TRANSACTION;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+SELECT * from animals;
+
+-- Transaction
+
+BEGIN TRANSACTION;
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT delete_2022_animals;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+ROLLBACK To delete_2022_animals;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+COMMIT;
+
+-- Queries
+
+SELECT count(*) FROM animals;
+SELECT count(*) FROM animals WHERE escape_attempts = 0;
+SELECT avg(weight_kg) FROM animals;
+
+SELECT CASE WHEN neutered THEN 'neutered' ELSE 'not neutered' END as escapes_most FROM animals
+GROUP BY neutered
+ORDER BY SUM(escape_attempts) DESC
+LIMIT 1;
+
+SELECT species, min(weight_kg), max(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, COALESCE(AVG(
+    CASE
+        WHEN date_of_birth BETWEEN '1990-01-01' AND '2000-01-01'
+            THEN escape_attempts
+    END), 0)
+FROM animals
+GROUP BY species;
+
