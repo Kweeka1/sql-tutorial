1d6e5018544891ef3d910429196517e3baca4c20
kweeka <mtalbi29@yahoo.com>
2023-01-18 11:11:31 +0100

    Add specie and owner tables

--- schema.sql
+++ schema.sql
@@ -10,3 +10,21 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+CREATE TABLE owners (
+     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+     full_name VARCHAR(50) NOT NULL,
+     age INT NOT NULL
+);
+
+CREATE TABLE species (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners(id);
+
